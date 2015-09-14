c8258d5aae5204a4c1027b793c4e6e4aa23deeb4
Rafał Solarski <solarski.rafal@gmail.com>
2015-09-14 13:23:35 +0200

    Game layout added.

---
+++ app/src/main/java/com/solarcode/solar/puzzle/GameActivity.java
@@ -0,0 +1,13 @@
+package com.solarcode.solar.puzzle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+public class GameActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
--- app/src/main/java/com/solarcode/solar/puzzle/MenuActivity.java
+++ app/src/main/java/com/solarcode/solar/puzzle/MenuActivity.java
@@ -1,37 +1,27 @@
 package com.solarcode.solar.puzzle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button mButton = (Button) findViewById(R.id.startButton);
+        mButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void onClick(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
     }
 }
